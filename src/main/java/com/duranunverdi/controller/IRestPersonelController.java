22c0aade2089960dadd9fe7ddf9974e7c32aa00a
DuranUnverdi <duranunverdi1905@gmail.com>
2026-02-07 22:59:33 +0300

    Refactoring-1

--- src/main/java/com/duranunverdi/controller/IRestPersonelController.java
+++ src/main/java/com/duranunverdi/controller/IRestPersonelController.java
@@ -1,10 +1,11 @@
 package com.duranunverdi.controller;
 
 import com.duranunverdi.model.Personel;
+import com.duranunverdi.utils.RestPageableRequest;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 public interface IRestPersonelController {
 
-    Page<Personel> findAll(Pageable pageable);
+    Page<Personel> findAll(RestPageableRequest restPageableRequest);
 }
--- src/main/java/com/duranunverdi/controller/impl/RestPersonelControllerImpl.java
+++ src/main/java/com/duranunverdi/controller/impl/RestPersonelControllerImpl.java
@@ -4,8 +4,11 @@ import com.duranunverdi.controller.IRestPersonelController;
 import com.duranunverdi.dto.DtoPersonel;
 import com.duranunverdi.model.Personel;
 import com.duranunverdi.service.IPersonelService;
+import com.duranunverdi.utils.RestPageableRequest;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,7 +24,8 @@ public class RestPersonelControllerImpl {
     }
 
     @GetMapping("/list/pageable")
-    public Page<DtoPersonel> findAll(Pageable pageable) {
+    public Page<DtoPersonel> findAll(RestPageableRequest restPageableRequest) {
+        Pageable pageable= PageRequest.of(restPageableRequest.getPageNumber(),restPageableRequest.getPageSize(),Sort.by(Sort.Direction.DESC,"id"));
         return service.findAll(pageable);
     }
 
